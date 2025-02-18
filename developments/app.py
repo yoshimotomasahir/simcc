e8c5588c92ee877fd8587af6f5ed380729d62632
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2025-02-18 15:50:55 +0900

    開発用 streamlit ファイルを developments 以下に移動

---
+++ developments/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="SimCC", page_icon="🔬")
+
+st.title("Welcome to SimCC 🔬")
+st.write("This application allows you to simulate charge-state changes in materials.")
+st.write("This page is under development.")
+
+st.sidebar.success("Select a page from the menu above.")
