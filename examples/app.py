bbf1f1f9d48a2019ba375a607815b0ee5967fb90
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2025-03-05 13:16:33 +0900

    Add repository information section with source code link in app.py

--- examples/app.py
+++ examples/app.py
@@ -5,4 +5,9 @@ st.set_page_config(page_title="SimCC", page_icon="🔬")
 st.title("Welcome to SimCC 🔬")
 st.write("This application allows you to simulate charge-state changes in materials.")
 
+st.write("## Repository Information")
+repo_url = "https://github.com/yoshimotomasahir/simcc.git"
+message = f"The source code is available [here]({repo_url})."
+st.write(message)
+
 st.sidebar.success("Select a page from the menu above.")
