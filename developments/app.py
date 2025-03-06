ddb415d208ca9505ae76175950570f3ab6249085
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2025-03-06 10:43:02 +0900

    fix: update page icon in Streamlit app

--- developments/app.py
+++ developments/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-st.set_page_config(page_title="SimCC", page_icon="🔬")
+st.set_page_config(page_title="SimCC", page_icon="🌠")
 
-st.title("Welcome to SimCC 🔬")
+st.title("Welcome to SimCC")
 st.write("This application allows you to simulate charge-state changes in materials.")
 st.write("This page is under development.")
 
