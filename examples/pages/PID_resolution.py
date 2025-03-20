902aeca31c9950b927580cd1c15a2de10d49edfd
Masahiro Yoshimoto <yoshimoto@onsanai.com>
2025-03-20 21:50:46 +0900

    feat: enhance PID resolution plot with Zdeg calculation

--- examples/pages/PID_resolution.py
+++ examples/pages/PID_resolution.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from uncertainties import ufloat
+import uncertainties.umath as umath
 import plotly.graph_objects as go
+import plotly
 
 import sys
 
@@ -19,7 +21,59 @@ with col2:
 with col3:
     Q = st.number_input("Charge State (Q)", value=50, step=1)
 with col4:
-    Energy = st.number_input("Energy", value=300, step=1)
+    Energy = st.number_input("Energy [MeV/u]", value=300, step=1)
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    deg_list = [
+        "Al 0.7 mm 0.61 mrad",
+        "Al 1.0 mm 0.43 mrad",
+        "Al 1.0 mm 0.95 mrad",
+        "Al 1.5 mm 1.187 mrad",
+        "Al 2.0 mm 1.60 mrad",
+        "Al 2.2 mm 1.80 mrad",
+        "Al 2.5 mm 2.14 mrad",
+        "Al 3.0 mm 2.62 mrad",
+        "Al 3.5 mm 2.80 mrad",
+        "Al 3.5 mm 3.00 mrad",
+        "Al 4.5 mm 3.70 mrad",
+        "Al 5.0 mm 4.25 mrad",
+        "Al 7.0 mm 5.969 mrad",
+        "Al 8.0 mm 7.3129 mrad",
+        "Al 9.0 mm 7.714 mrad",
+        "Al 10.0 mm 8.587 mrad",
+    ]
+    F5_deg = st.selectbox("F5 degrader", options=deg_list, index=4)
+    F5_deg_center = float(F5_deg.split()[1]) * 0.1  # cm
+    F5_deg_angle = float(F5_deg.split()[3])  # mrad
+with col2:
+    F5_deg_error = st.selectbox(
+        "Thickness ununiformity",
+        options=[
+            "0 μm",
+            "1 μm",
+            "2 μm",
+            "5 μm",
+            "10 μm",
+            "15 μm",
+            "20 μm",
+            "25 μm",
+        ],
+        index=4,
+    )
+    F5_deg_error = float(F5_deg_error.split()[0]) * 0.1 * 0.001
+with col3:
+    Straggling_enhancement = st.number_input("Straggling enhancement by cc", value=1.0, step=0.1, min_value=1.0)
+
+
+eloss, straggling = GetAnalyticalEloss(A, Z, Energy, "Al", F5_deg_center)
+F5_deg_thickness = ufloat(F5_deg_center, F5_deg_error)
+F5_deg_thickness *= ufloat(1, straggling / eloss * Straggling_enhancement)
+
+energy35_nominal = Energy
+energy57_nominal = Energy - eloss
+brho35_nominal = energy2brho(energy35_nominal, A, Q)
+brho57_nominal = energy2brho(energy57_nominal, A, Q)
 
 
 def calc_delta_ai(Xi, Xf, Af, mat):
@@ -97,6 +151,7 @@ TOF_error = np.linspace(10, 120, 12)
 X, Y = np.meshgrid(PPAC_error, TOF_error)
 AOQ35s = np.zeros_like(X)
 AOQ57s = np.zeros_like(X)
+Zdegs = np.zeros_like(X)
 
 
 def focus2int(FaFb):
@@ -109,6 +164,20 @@ for FaFb in ["F3-F5", "F7-F5"]:
     if FaFb not in st.session_state.matrix:
         st.session_state.matrix[FaFb] = get_matrix(*focus2int(FaFb))
 
+
+def calc_zdeg(F5X, F5A, beta35, beta57, brho35, brho57, d0, angle, Z):
+    zpos = 0
+    pos = F5X + zpos * umath.tan(F5A / 1000)
+    t = d0 + pos * angle / 1000
+    t = t / umath.cos(F5A / 1000)
+    ionpair = 6156  # Al
+    de_v = t * (umath.log(ionpair * beta35 * beta35) - umath.log(1 - beta35 * beta35) - beta35 * beta35)
+    delE = brho35 / beta35 - brho57 / beta57
+    a = Z / ((delE / de_v) * beta35 * beta35)
+    zdeg = a.n * (delE / de_v) * beta35 * beta35
+    return zdeg
+
+
 for i, j in np.ndindex(X.shape):
     xval = X[i, j]
     F3X = ufloat(0, xval)
@@ -120,29 +189,35 @@ for i, j in np.ndindex(X.shape):
     F3TOF = ufloat(0, yval / 1000)
     F7TOF = ufloat(0, yval / 1000)
 
-    energy35 = Energy
-    energy57 = Energy
-    brho35 = energy2brho(energy35, A, Q)
-    brho57 = energy2brho(energy57, A, Q)
-
     delta35, _ = calc_delta_ai(F3X, F5X, F5A, st.session_state.matrix["F3-F5"])
     delta57, _ = calc_delta_ai(F7X, F5X, F5A, st.session_state.matrix["F7-F5"])
 
     fl35 = calc_fl(F3X, F3A, delta35, st.session_state.matrix["F3-F5"], get_flc(3, 5))
     fl57 = calc_fl(F7X, F7A, delta57, st.session_state.matrix["F7-F5"], get_flc(5, 7))
 
-    TOF35 = beta2tof(fl35, energy2beta(energy35))
+    TOF35 = beta2tof(fl35, energy2beta(energy35_nominal))
     TOF35 += F3TOF
-    TOF57 = beta2tof(fl57, energy2beta(energy57))
+    TOF57 = beta2tof(fl57, energy2beta(energy57_nominal))
     TOF57 += F7TOF
     TOF37 = TOF35 + TOF57
 
-    AOQ35, AOQ57, _, _, _, _ = calcAOQ_from_2brho(brho35 * (1 + delta35 * 0.01), brho57 * (1 + delta57 * 0.01), fl35, fl57, TOF37)
+    brho35 = brho35_nominal * (1 + delta35 * 0.01)
+    brho57 = brho57_nominal * (1 + delta57 * 0.01)
+    AOQ35, AOQ57, beta35, beta57, _, _ = calcAOQ_from_2brho(brho35, brho57, fl35, fl57, TOF37)
+    Zdeg = calc_zdeg(F5X, F5A, beta35, beta57, brho35, brho57, F5_deg_thickness, F5_deg_angle, Z)
+
     AOQ35s[i, j] = AOQ35.s / AOQ35.n * 100
     AOQ57s[i, j] = AOQ57.s / AOQ35.n * 100
+    Zdegs[i, j] = Zdeg.s
 
-heatmap = go.Heatmap(z=AOQ35s, x=PPAC_error, y=TOF_error, colorscale="Viridis", opacity=0.7)
-contour = go.Contour(z=AOQ35s, x=PPAC_error, y=TOF_error, colorscale="Blues", contours=dict(showlabels=True), contours_coloring="lines")
-fig = go.Figure(data=[heatmap, contour])
-fig.update_layout(title="A/Q resolution [%]", xaxis_title="PPAC resolution [mm]", yaxis_title="TOF resolution [ps]")
+heatmap1 = go.Heatmap(z=AOQ35s, x=PPAC_error, y=TOF_error, colorscale="Viridis", opacity=0.7)
+contour1 = go.Contour(z=AOQ35s, x=PPAC_error, y=TOF_error, colorscale="Blues", contours=dict(showlabels=True), contours_coloring="lines")
+fig = go.Figure(data=[heatmap1, contour1])
+fig.update_layout(title="A/Q resolution [%] (std.dev.)", xaxis_title="PPAC resolution [mm]", yaxis_title="TOF resolution [ps]",margin=dict(l=5, r=5, t=30, b=5),width=1000,height=300)
+st.plotly_chart(fig)
+
+heatmap2 = go.Heatmap(z=Zdegs, x=PPAC_error, y=TOF_error, colorscale="Viridis", opacity=0.7)
+contour2 = go.Contour(z=Zdegs, x=PPAC_error, y=TOF_error, colorscale="Blues", contours=dict(showlabels=True), contours_coloring="lines")
+fig = go.Figure(data=[heatmap2, contour2])
+fig.update_layout(title="Zdeg resolution (std.dev.)", xaxis_title="PPAC resolution [mm]", yaxis_title="TOF resolution [ps]",margin=dict(l=5, r=5, t=30, b=5),width=1000,height=300)
 st.plotly_chart(fig)
