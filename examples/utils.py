fix: update input handling for projectile parameters and energy units in input_projectile function

examples/utils.py
@@ -125,31 +125,42 @@ def get_matrix(Fa, Fb):
 
 def input_projectile():
     st.write("**Projectile**: Configure the projectile parameters as the initial state for the calculations.")
-    use_number_input = st.toggle("Number input")
+    use_number_input = st.toggle("Number input", value=True)
 
     col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
-    with col1:
+    with col2:
         if use_number_input:
             projectile_Z = st.number_input("Z", min_value=30, max_value=94, step=1, value=70)
         else:
             projectile_Z = st.slider("Z", 30, 94, 70)
         st.write(f"Element: {z2symbol[projectile_Z]}")
-    with col2:
+    with col4:
+        energy_unit = st.radio("Energy unit", ["MeV/u", "Tm"], horizontal=True)
         if use_number_input:
-            energy = st.number_input("Energy (MeV/u)", min_value=50.0, max_value=1000.0, step=5.0, value=250.0)
+            if energy_unit == "MeV/u":
+                energy = st.number_input("Energy (MeV/u)", min_value=50.0, max_value=1000.0, step=5.0, value=250.0)
+            elif energy_unit == "Tm":
+                brho = st.number_input("Energy (Tm)", min_value=2.0, max_value=20.0, step=0.1, value=6.0)
         else:
-            energy = st.slider("Energy (MeV/u)", 50, 1000, 250, step=5)
-    with col3:
+            if energy_unit == "MeV/u":
+                energy = st.slider("Energy (MeV/u)", 50, 1000, 250, step=5)
+            elif energy_unit == "Tm":
+                brho = st.number_input("Energy (Tm)", min_value=2.0, max_value=20.0, step=0.1, value=6.0)
+    with col1:
         if use_number_input:
             A = st.number_input("A", min_value=50, max_value=300, step=1, value=175)
         else:
             AoZ = st.slider("A/Z", 1.5, 3.5, 2.5, step=0.01)
             A = int(np.round(projectile_Z * AoZ))
             st.write(f"A={A}")
-    with col4:
+    with col3:
         charge_states = {0: "Full-strip", 1: "H-like", 2: "He-like", 3: "Li-like", 4: "Be-like", 5: "B-like", 6: "C-like"}
         charge_state = st.selectbox("Charge states", options=list(charge_states.keys()), format_func=lambda x: charge_states[x])
-        st.write(f"Brho={energy2brho(energy, A, projectile_Z-charge_state):.6f} Tm")
+        if energy_unit == "MeV/u":
+            st.write(f"{energy2brho(energy, A, projectile_Z-charge_state):.6f} Tm")
+        else:
+            energy = brho2energy(brho, A, projectile_Z-charge_state)
+            st.write(f"{energy:.6f} MeV/u")
 
     return projectile_Z, energy, A, charge_state
 